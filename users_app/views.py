e09a12605edb9390e007e27af48679324ab8d0a1
Anas Zughayyar <anaszughayyar@neobook.local>
2024-11-05 21:36:38 +0200

    update assignment

--- users_app/views.py
+++ users_app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import User
 
 
@@ -8,3 +8,18 @@ def index(request):
         'users' : User.objects.all()
     }
     return render(request, "index.html" , context)
+
+def add(request):
+    first_name = request.POST['first_name']
+    last_name = request.POST['last_name']
+    email_address = request.POST['email_address']
+    age = request.POST['age']
+    User.objects.create(first_name = first_name, last_name = last_name , email_address = email_address , age = age)
+    return redirect('/')
+
+def remove(request):
+    user_id = request.POST['id']
+    c = User.objects.get(id = user_id)
+    print("value of c" , c)
+    c.delete()
+    return redirect('/')
